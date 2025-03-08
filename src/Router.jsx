c2f73cad3920ef73fe82469af63e13b020dfcb0f
최권진 <rnjswls060@naver.com>
2025-03-08 11:01:01 +0900

    feat: 라우터 설정 추가 React Router를 사용하여 기본적인 페이지 라우팅을 설정했습니다. Router.jsx에서 모든 경로에 대한 Route를 정의했습니다. Pages 폴더를 생성하고, HomePage, ListPage, PostPage 등 주요 페이지 컴포넌트를 추가했습니다. App.jsx에서 Router를 impoert하여 라우팅을 적용했습니다.

---
+++ src/Router.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ListPage from "./pages/ListPage";
+import PostPage from "./pages/PostPage";
+import PostItemPage from "./pages/PostItemPage";
+import EditPage from "./pages/EditPage";
+import MessagePage from "./pages/MessagePage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/list" element={<ListPage />} />
+        <Route path="/post" element={<PostPage />} />
+        <Route path="/post/:id" element={<PostItemPage />} />
+        <Route path="/post/:id/edit" element={<EditPage />} />
+        <Route path="/post/:id/message" element={<MessagePage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
