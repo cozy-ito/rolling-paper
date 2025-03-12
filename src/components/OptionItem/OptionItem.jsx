76cafa016201651351562a7747d6ebe9f8a5e94b
최권진 <rnjswls060@naver.com>
2025-03-12 13:03:37 +0900

    fix(common): OptionItem 컴포넌트 수정 - 중복된 체크 아이콘 렌더링 로직 제거 - children prop을 활용 -> color, image, checkActive 제거

--- src/components/OptionItem/OptionItem.jsx
+++ src/components/OptionItem/OptionItem.jsx
@@ -1,5 +1,6 @@
-import styles from "./OptionItem.module.css";
 import checkIcon from "../../assets/icons/check.svg";
+
+import styles from "./OptionItem.module.css";
 
 const OptionItem = ({ children, isSelected, className, onClick }) => {
   return (
