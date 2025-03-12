71304089d08fa293943bc8d76f13f186c2f11e6c
jeonghwanJay <nawhgnoejij@naver.com>
2025-03-12 18:01:41 +0900

    refactor(badge): 텍스트 색상 제어 가능하도록 Badge 컴포넌트 수정
    - `BADGE_COLORS`에서 텍스트 색상을 관리하도록 개선

--- src/components/Badge/Badge.jsx
+++ src/components/Badge/Badge.jsx
@@ -1,29 +1,63 @@
 import styles from "./Badge.module.css";
 
 const Badge = ({ text, color }) => {
+  const { backgroundColor, color: textColor } = BADGE_COLORS[color];
+
   return (
     <span
       className={styles.badge}
-      style={{ backgroundColor: BADGE_COLORS[color] }}
-    >
+      style={{ backgroundColor, color: textColor }}>
       {text}
     </span>
   );
 };
 
 const BADGE_COLORS = {
-  primary: "var(--color-blue-100)",
-  secondary: "var(--color-gray-100)",
-  success: "var(--color-green-100)",
-  warning: "var(--color-beige-500)",
-  danger: "var(--color-error)",
-  info: "var(--color-green-300)",
+  primary: {
+    backgroundColor: "var(--color-blue-100)",
+    color: "var(--color-blue-500)",
+  },
+  secondary: {
+    backgroundColor: "var(--color-gray-100)",
+    color: "var(--color-gray-800)",
+  },
+  success: {
+    backgroundColor: "var(--color-green-100)",
+    color: "var(--color-green-500)",
+  },
+  warning: {
+    backgroundColor: "var(--color-beige-500)",
+    color: "var(--color-error)",
+  },
+  danger: {
+    backgroundColor: "var(--color-error)",
+    color: "var(--color-white)",
+  },
+  info: {
+    backgroundColor: "var(--color-green-300)",
+    color: "var(--color-green-900)",
+  },
 
-  like: "var(--color-green-200)",
-  dislike: "var(--color-gray-400)",
-  new: "var(--color-purple-100)",
-  premium: "var(--color-beige-200)",
-  verified: "var(--color-blue-200)",
+  like: {
+    backgroundColor: "var(--color-green-200)",
+    color: "var(--color-green-500)",
+  },
+  dislike: {
+    backgroundColor: "var(--color-gray-400)",
+    color: "var(--color-gray-900)",
+  },
+  new: {
+    backgroundColor: "var(--color-purple-100)",
+    color: "var(--color-purple-900)",
+  },
+  premium: {
+    backgroundColor: "var(--color-beige-200)",
+    color: "var(--color-beige-500)",
+  },
+  verified: {
+    backgroundColor: "var(--color-blue-200)",
+    color: "var(--color-blue-500)",
+  },
 };
 
 export default Badge;
