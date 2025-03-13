2e1e8abfd9481b74f0a58a8fd36c7c4ec3c4086f
cozy-ito <cozy.ito.dev@gmail.com>
2025-03-13 17:37:41 +0900

    feat(common): Header 컴포넌트 구현

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,33 +1,57 @@
+import React from "react";
+
 import clsx from "clsx";
+import { Link, useLocation } from "react-router-dom";
+
+import Logo from "../../assets/icons/logo.svg";
 
 import styles from "./Header.module.css";
 
-const ResponsiveElement = ({
-  children,
-  visibleOn = ["mobile", "tablet", "desktop"],
-}) => {
-  const hiddenOnMobile = !visibleOn.includes("mobile");
-  const hiddenOnTablet = !visibleOn.includes("tablet");
-  const hiddenOnDesktop = !visibleOn.includes("desktop");
+const BREAKPOINTS = {
+  DESKTOP: "desktop",
+  TABLET: "tablet",
+  MOBILE: "mobile",
+};
+
+const ALL_BREAKPOINTS = Object.values(BREAKPOINTS);
+
+const ResponsiveElement = ({ children, visibleOn = ALL_BREAKPOINTS }) => {
+  const hiddenOnDesktop = !visibleOn.includes(BREAKPOINTS.DESKTOP);
+  const hiddenOnTablet = !visibleOn.includes(BREAKPOINTS.TABLET);
+  const hiddenOnMobile = !visibleOn.includes(BREAKPOINTS.MOBILE);
 
   return (
-    <div
+    <React.Fragment
       className={clsx({
-        [styles.hidden_on_desktop]: hiddenOnDesktop,
-        [styles.hidden_on_tablet]: hiddenOnTablet,
-        [styles.hidden_on_mobile]: hiddenOnMobile,
+        [styles.hiddenOnDesktop]: hiddenOnDesktop,
+        [styles.hiddenOnTablet]: hiddenOnTablet,
+        [styles.hiddenOnMobile]: hiddenOnMobile,
       })}
     >
       {children}
-    </div>
+    </React.Fragment>
   );
 };
 
-const Header = ({ visibleOn }) => {
+const showButtonPageList = ["/", "/list"];
+
+const Header = ({ visibleOn = ALL_BREAKPOINTS }) => {
+  const { pathname } = useLocation();
+  const isShow = showButtonPageList.includes(pathname);
+
   return (
     <ResponsiveElement visibleOn={visibleOn}>
       <div className={styles.container}>
-        <div className={styles.inner_container}>Header</div>
+        <div className={styles.innerContainer}>
+          <Link to="/">
+            <img src={Logo} alt="로고" />
+          </Link>
+          {isShow && (
+            <Link to="/post">
+              <button className={styles.button}>롤링 페이퍼 만들기</button>
+            </Link>
+          )}
+        </div>
       </div>
     </ResponsiveElement>
   );
