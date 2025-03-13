70fda67fe72b71b37134562d168ee9d4a2638e98
jeonghwanJay <nawhgnoejij@naver.com>
2025-03-14 01:20:59 +0900

    refactor(Input): props 최적화 및 disabled 속성 변경
    - 기존에 있던 placeholder, value, onChange 등을 rest parameter에 포함시켜 props 최적화
    - disabled 속성을 input과 className에 전달하지 않고, 가상 선택자를 사용하여 스타일링

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,26 +1,13 @@
 import styles from "./Input.module.css";
 import clsx from "clsx";
 
-const Input = ({
-  placeholder = "placeholder",
-  disabled = false,
-  isError = false,
-  errorMessage = "",
-  value,
-  onChange,
-  ...props
-}) => {
+const Input = ({ isError = false, errorMessage = "", ...props }) => {
   return (
     <div>
       <input
         className={clsx(styles.input, {
           [styles.isError]: isError,
-          [styles.disabled]: disabled,
         })}
-        value={value}
-        onChange={onChange}
-        placeholder={placeholder}
-        disabled={disabled}
         {...props}
       />
       {isError && errorMessage && (
