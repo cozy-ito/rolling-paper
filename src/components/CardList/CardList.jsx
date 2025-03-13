a48f00f194b8790509b7625c426fc8b0ae3140c4
zeon0xx0 <jihyon5615@naver.com>
2025-03-14 07:26:22 +0900

    fix(common): 코드 전체 수정

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,32 +1,42 @@
-import clsx from "clsx";
-
-import Card from "../Card/Card";
+import Badge from "../Badge/Badge";
 
 import styles from "./CardList.module.css";
 
-const CardList = ({ cards = [], className, onCardClick, ...props }) => {
+const CardList = ({
+  background,
+  users,
+  message,
+  badges = [],
+  onClick,
+  userCountText,
+}) => {
   return (
-    <div className={clsx(styles.cardList, className)}>
-      {cards.map((card) => (
-        <Card
-          key={card.id}
-          className={clsx(
-            styles.card,
-            card.image ? styles.withImage : styles.background,
+    <div className={styles.card} style={{ background }} onClick={onClick}>
+      <div className={styles.cardMessage}>{message}</div>
+      <div className={styles.profileContainer}>
+        <div className={styles.profileImage}>
+          <div className={styles.userCount}>{users.length}</div>
+          {users.slice(0, 3).map((user) => (
+            <img
+              key={user.id}
+              src={user.avatar}
+              alt={user.name}
+              className={styles.profileImages}
+            />
+          ))}
+
+          {users.length > 3 && (
+            <div className={styles.extraCount}>+{users.length - 3}</div>
           )}
-          id={card.id}
-          type={card.type}
-          image={card.image}
-          profileImages={card.profileImages}
-          message={card.message}
-          title={card.title}
-          badge={card.badge}
-          prefix={card.prefix}
-          content={card.content}
-          onClick={() => onCardClick(card.id)}
-          {...props}
-        />
-      ))}
+        </div>
+        <div className={styles.userCountText}>
+          {userCountText
+            ? userCountText.replace("{count}", users.length)
+            : `${users.length}명이 작성했어요!`}
+
+          <Badge badges={badges} />
+        </div>
+      </div>
     </div>
   );
 };
