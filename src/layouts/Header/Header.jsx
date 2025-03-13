57d54474e9130c65399f2ceb4b789efa314e1d60
cozy-ito <cozy.ito.dev@gmail.com>
2025-03-13 17:37:41 +0900

    feat(common): Header 컴포넌트 구현

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,16 +1,58 @@
+import React from "react";
+
 import clsx from "clsx";
+import { Link, useLocation } from "react-router-dom";
+
+import Logo from "../../assets/icons/logo.svg";
+import { ROUTES } from "../../constants/routes";
+
 import styles from "./Header.module.css";
 
-const Header = ({ mobileHidden }) => {
+const Header = ({ visibleOn = ALL_BREAKPOINTS }) => {
+  const { pathname } = useLocation();
+  const showCreateButton = ROUTES_WITH_CREATE_BUTTON.includes(pathname);
+
+  return (
+    <ResponsiveElement visibleOn={visibleOn}>
+      <div className={styles.innerContainer}>
+        <Link to={ROUTES.HOME}>
+          <img src={Logo} alt="로고" />
+        </Link>
+        {showCreateButton && (
+          <Link to={ROUTES.POST}>
+            <button className={styles.button}>롤링 페이퍼 만들기</button>
+          </Link>
+        )}
+      </div>
+    </ResponsiveElement>
+  );
+};
+
+const ResponsiveElement = ({ children, visibleOn = ALL_BREAKPOINTS }) => {
+  const hiddenOnDesktop = !visibleOn.includes(BREAKPOINTS.DESKTOP);
+  const hiddenOnTablet = !visibleOn.includes(BREAKPOINTS.TABLET);
+  const hiddenOnMobile = !visibleOn.includes(BREAKPOINTS.MOBILE);
+
   return (
     <div
       className={clsx(styles.container, {
-        [styles.mobile_hidden]: mobileHidden,
+        [styles.hiddenOnDesktop]: hiddenOnDesktop,
+        [styles.hiddenOnTablet]: hiddenOnTablet,
+        [styles.hiddenOnMobile]: hiddenOnMobile,
       })}
     >
-      <div className={styles.inner_container}>Header</div>
+      {children}
     </div>
   );
 };
+
+const BREAKPOINTS = {
+  DESKTOP: "desktop",
+  TABLET: "tablet",
+  MOBILE: "mobile",
+};
+
+const ALL_BREAKPOINTS = Object.values(BREAKPOINTS);
+const ROUTES_WITH_CREATE_BUTTON = [ROUTES.HOME, ROUTES.LIST];
 
 export default Header;
