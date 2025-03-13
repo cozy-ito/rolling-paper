822463d108d21865ae782799a513ca3795ee54ed
최권진 <rnjswls060@naver.com>
2025-03-13 21:36:46 +0900

    feat(common): Modal 공통 컴포넌트 구현 모달 컴포넌트를 구현하여 배경 오버레이, 내부 클릭 이벤트 차단 등의 기능을 추가함 프로필 이미지, 제목, 배지, 날짜, 본문 등의 props를 받아 렌더링하도록 구성함

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,39 @@
+import styles from "./Modal.module.css";
+
+const Modal = ({
+  isModalOpen,
+  onClose,
+  profileImg,
+  title,
+  badge,
+  date,
+  bodyText,
+}) => {
+  if (!isModalOpen) return null;
+  return (
+    <div className={isModalOpen ? styles.modalOverlay : ""} onClick={onClose}>
+      <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
+        <div className={styles.modalHeader}>
+          <div className={styles.modalHeaderUserInfo}>
+            <img src={profileImg} alt="profile image" className={styles.img} />
+            <div className={styles.modalHeaderUserTextInfo}>
+              <h3 className={styles.modalHeaderTitle}>
+                <span className={styles.modalHeaderFrom}>from.</span>
+                {title}
+              </h3>
+              {badge && badge}
+            </div>
+          </div>
+          <p className={styles.date}>{date}</p>
+        </div>
+        <hr className={styles.divider} />
+        <p className={styles.bodyText}>{bodyText}</p>
+        <button onClick={onClose} className={styles.button}>
+          확인
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,25 @@
-import ArrowButton from "../components/ArrowButton/ArrowButton";
+import { useState } from "react";
+
+import profileImg from "../assets/icons/profile-1.svg";
+import Modal from "../components/Modal/Modal";
 
 const HomePage = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <div>
-      <ArrowButton direction="top" />
+      <button onClick={() => setIsModalOpen(true)}>모달 등장</button>
+      <Modal
+        isModalOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        profileImg={profileImg}
+        title={"제목"}
+        badge={"동료"}
+        date={"2025-03-13"}
+        bodyText={
+          "오늘 날씨가 좋네요/ 오늘 날씨가 좋네요/ 오늘 날씨가 좋네요/ 오늘 날씨가 좋네요/ 오늘 날씨가 좋네요/ 오늘 날씨가 좋네요/ 오늘 날씨가 좋네요/ 오늘 날씨가 좋네요/ 오늘 날씨가 좋네요/ 오늘 날씨가 좋네요/ 오늘 날씨가 좋네요/ 오늘 날씨가 좋네요/ 오늘 날씨가 좋네요/ 오늘 날씨가 좋네요/ 오늘 날씨가 좋네요/ 오늘 날씨가 좋네요/ 오늘 날씨가 좋네요/ 오늘 날씨가 좋네요/ 오늘 날씨가 좋네요/ 오늘 날씨가 좋네요/ 오늘 날씨가 좋네요/ 오늘 날씨가 좋네요/ 오늘 날씨가 좋네요/오늘 날씨가 좋네요/ 오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/오늘 날씨가 좋네요/ "
+        }
+      />
     </div>
   );
 };
