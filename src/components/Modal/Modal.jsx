5c0a5d28cef0780a1b166ffdb75ec6ae4ee07d38
최권진 <rnjswls060@naver.com>
2025-03-14 13:48:34 +0900

    fix(common): Modal 컴포넌트 수정 기존 삼항연산자로 불필요하게 조건부 렌더링 하던 코드를 없앴습니다. 기존 하드코딩 되어있던 from.을fromLabel prop으로 받도록 변경했습니다.

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -8,17 +8,18 @@ const Modal = ({
   badge,
   date,
   bodyText,
+  fromLabel,
 }) => {
   if (!isModalOpen) return null;
   return (
-    <div className={isModalOpen ? styles.modalOverlay : ""} onClick={onClose}>
+    <div className={styles.modalOverlay} onClick={onClose}>
       <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
         <div className={styles.modalHeader}>
           <div className={styles.modalHeaderUserInfo}>
             <img src={profileImg} alt="profile image" className={styles.img} />
             <div className={styles.modalHeaderUserTextInfo}>
               <h3 className={styles.modalHeaderTitle}>
-                <span className={styles.modalHeaderFrom}>from.</span>
+                <span className={styles.modalHeaderFrom}>{fromLabel}</span>
                 {title}
               </h3>
               {badge && badge}
