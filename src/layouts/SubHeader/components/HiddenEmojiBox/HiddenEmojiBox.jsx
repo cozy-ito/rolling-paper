d4a0be01662f5cd025e35499ae4a4a69904bfd11
cozy-ito <cozy.ito.dev@gmail.com>
2025-03-14 15:50:28 +0900

    feat(header) HiddenEmojiBox 컴포넌트 구현 - Header 컴포넌트에서만 사용할 것으로 예상되어 Header/components 폴더에 위치

---
+++ src/layouts/SubHeader/components/HiddenEmojiBox/HiddenEmojiBox.jsx
@@ -0,0 +1,30 @@
+import ArrowDownIcon from "../../../../assets/icons/arrow-down.svg";
+import PopoverWrapper from "../../../../components/PopoverWrapper/PopoverWrapper";
+
+import styles from "./HiddenEmojiBox.module.css";
+
+const HiddenEmojiBox = ({ emojiList }) => {
+  return (
+    <PopoverWrapper>
+      {({ targetRef, isToggle, setIsToggle }) => (
+        <>
+          <button type="button" onClick={() => setIsToggle((prev) => !prev)}>
+            <img src={ArrowDownIcon} alt="아래방향 화살표" />
+          </button>
+          {isToggle && (
+            <ul ref={targetRef} className={styles.emojiHiddenList}>
+              {emojiList.map(({ emoji, count }, index) => (
+                <li key={index} className={styles.badge}>
+                  <span>{emoji}</span>
+                  <span>{count}</span>
+                </li>
+              ))}
+            </ul>
+          )}
+        </>
+      )}
+    </PopoverWrapper>
+  );
+};
+
+export default HiddenEmojiBox;
