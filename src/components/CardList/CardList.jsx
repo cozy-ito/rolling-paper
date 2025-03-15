6de987c736e02a85a9e2e993bc136c93ad2bdd5a
zeon0xx0 <jihyon5615@naver.com>
2025-03-15 20:41:16 +0900

    fix(common): CardList 공통 컴포넌트 수정

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,74 +1,64 @@
 //import Badge from "../Badge/Badge";
 
-import blueBg from "../../assets/imgs/blue.png";
-import greenBg from "../../assets/imgs/green.png";
-import orangeBg from "../../assets/imgs/orange.png";
-import purpleBg from "../../assets/imgs/purple.png";
+import blue from "../../assets/imgs/blue.png";
+import green from "../../assets/imgs/green.png";
+import orange from "../../assets/imgs/orange.png";
+import purple from "../../assets/imgs/purple.png";
 
 import styles from "./CardList.module.css";
 
-const getBackgroundImage = (background) => {
-  switch (background) {
-    case "#FFE2AD":
-      return orangeBg;
-    case "#ECD9FF":
-      return purpleBg;
-    case "#B1E4FF":
-      return blueBg;
-    case "#D0F5C3":
-      return greenBg;
-    default:
-      return null;
-  }
-};
-
-const CardList = ({ background, users, message, badges = [], onClick }) => {
-  const maxUsers = 3;
-  const extraCount = users.length - maxUsers;
-  const backgroundImage = getBackgroundImage(background);
+const CardList = ({
+  backgroundColor,
+  profileSection,
+  totalUsers = 0,
+  message,
+  userMessage,
+  badges = [],
+  onClick,
+}) => {
+  const backgroundImage = getBackgroundImage(backgroundColor);
 
   return (
-    <div className={styles.card} style={{ background }} onClick={onClick}>
+    <div
+      className={styles.card}
+      style={{
+        background: backgroundColor,
+        backgroundImage: backgroundImage ? `url(${backgroundImage})` : "none",
+        backgroundSize: "50% auto",
+        backgroundPosition: "bottom right",
+        backgroundRepeat: "no-repeat",
+      }}
+      onClick={onClick}
+    >
       <div className={styles.cardMessage}>{message}</div>
-      <div className={styles.profileContainer}>
-        <div className={styles.profileWrapper}>
-          <div className={styles.profileImage}>
-            {users.slice(0, maxUsers).map((user) => (
-              <img
-                key={user.id}
-                src={user.avatar}
-                alt={user.name}
-                className={styles.profileImages}
-              />
-            ))}
-            {extraCount > 0 && (
-              <div className={styles.extraCount}>+{extraCount}</div>
-            )}
-          </div>
-        </div>
-        <div className={styles.userCount}>
-          <span className={styles.userCountNumber}>{users.length}</span>명이
-          작성했어요!
-        </div>
+
+      {profileSection}
+
+      <div className={styles.userCount}>
+        <span className={styles.userCountNumber}>{totalUsers}</span>
+        {userMessage}
       </div>
+
       <div className={styles.reactionsWrapper}>
         <div className={styles.reactionsContainer}>
           {badges.map((badge, index) => (
-            <div key={index} className={styles.reactionBadge}>
-              <span className="emoji">{badge.emoji}</span>
-              <span className="count">{badge.count}</span>
-            </div>
+            <div key={index} {...badge} />
           ))}
         </div>
       </div>
-      {backgroundImage && (
-        <div
-          className={styles.shadow}
-          style={{ backgroundImage: `url(${backgroundImage})` }}
-        />
-      )}
     </div>
   );
+};
+
+const getBackgroundImage = (backgroundColor) => {
+  const backgroundImages = {
+    "#FFE2AD": orange,
+    "#ECD9FF": purple,
+    "#B1E4FF": blue,
+    "#D0F5C3": green,
+  };
+
+  return backgroundImages[backgroundColor];
 };
 
 export default CardList;
