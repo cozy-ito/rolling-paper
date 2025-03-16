de6ad8edf1c9c69657cd2e9eb70199e50f30250f
cozy-ito <cozy.ito.dev@gmail.com>
2025-03-16 21:13:49 +0900

    feat(header) ShareButton UI 구현

--- src/components/ShareButton/ShareButton.jsx
+++ src/components/ShareButton/ShareButton.jsx
@@ -1,14 +1,33 @@
-import React from "react";
-
 import ShareIcon from "../../assets/icons/share.svg";
+import PopoverWrapper from "../PopoverWrapper/PopoverWrapper";
 
 import styles from "./ShareButton.module.css";
 
 const ShareButton = () => {
   return (
-    <button type="button" className={styles.button}>
-      <img src={ShareIcon} alt="공유하기 아이콘" />
-    </button>
+    <PopoverWrapper>
+      {({ popoverRef, isOpen, setIsOpen }) => (
+        <div className={styles.container}>
+          <button
+            type="button"
+            className={styles.button}
+            onClick={() => setIsOpen((prev) => !prev)}
+          >
+            <img src={ShareIcon} alt="공유하기 아이콘" />
+          </button>
+          {isOpen && (
+            <ul ref={popoverRef} className={styles.dropdown}>
+              <li>
+                <button type="button">카카오톡 공유</button>
+              </li>
+              <li>
+                <button type="button">URL 공유</button>
+              </li>
+            </ul>
+          )}
+        </div>
+      )}
+    </PopoverWrapper>
   );
 };
 
