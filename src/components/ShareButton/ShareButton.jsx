b98ca77b557d314d642a81f3cf4065678c1a6eb7
cozy-ito <cozy.ito.dev@gmail.com>
2025-03-16 21:14:31 +0900

    feat(header): URL 공유 기능 추가

--- src/components/ShareButton/ShareButton.jsx
+++ src/components/ShareButton/ShareButton.jsx
@@ -1,9 +1,27 @@
+import React, { useState } from "react";
+
 import ShareIcon from "../../assets/icons/share.svg";
 import PopoverWrapper from "../PopoverWrapper/PopoverWrapper";
+import Toast from "../Toast/Toast";
 
 import styles from "./ShareButton.module.css";
 
 const ShareButton = () => {
+  const [isError, setIsError] = useState(false);
+  const [isToastVisible, setIsToastVisible] = useState(false);
+
+  const handleClickCopyUrl = async () => {
+    try {
+      setIsError(false);
+      await navigator.clipboard.writeText(location.href);
+    } catch (error) {
+      setIsError(true);
+      console.error("클립보드 복사가 실패하였습니다.", error);
+    } finally {
+      setIsToastVisible(true);
+    }
+  };
+
   return (
     <PopoverWrapper>
       {({ popoverRef, isOpen, setIsOpen }) => (
@@ -21,10 +39,20 @@ const ShareButton = () => {
                 <button type="button">카카오톡 공유</button>
               </li>
               <li>
-                <button type="button">URL 공유</button>
+                <button type="button" onClick={handleClickCopyUrl}>
+                  URL 공유
+                </button>
               </li>
             </ul>
           )}
+          <Toast
+            isVisible={isToastVisible}
+            setIsVisible={setIsToastVisible}
+            duration={3000}
+            message={
+              isError ? "URL 복사가 실패하였습니다. " : "URL이 복사되었습니다."
+            }
+          />
         </div>
       )}
     </PopoverWrapper>
