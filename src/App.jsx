1eabbe4a16b31346020a61cdcf1dbd8cd30f6a38
zeon0xx0 <jihyon5615@naver.com>
2025-03-17 01:09:56 +0900

    App.jsx 변경 사항 반영

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/*
 import Router from "./Router";
 
 const App = () => {
@@ -11,4 +12,39 @@ const App = () => {
     </>
   );
 };
+export default App;
+
+*/
+
+import ProfileButton from "./components/ProfileButton/ProfileButton";
+
+const App = () => {
+  const handleClick = (name) => {
+    alert(`${name}님의 프로필 클릭!`);
+  };
+
+  return (
+    <div style={{ padding: "20px" }}>
+      {/* 작은 프로필 버튼들 */}
+      <div style={{ display: "flex", gap: "10px", marginBottom: "20px" }}>
+        <ProfileButton onClick={() => handleClick("유저1")}>
+          유저1
+        </ProfileButton>
+        <ProfileButton onClick={() => handleClick("유저2")}>
+          유저2
+        </ProfileButton>
+        <ProfileButton onClick={() => handleClick("유저3")}>
+          유저3
+        </ProfileButton>
+        <ProfileButton onClick={() => handleClick("유저4")}>
+          유저4
+        </ProfileButton>
+      </div>
+
+      {/* 큰 프로필 버튼 */}
+      <ProfileButton size="large" onClick={() => handleClick("메인 유저")} />
+    </div>
+  );
+};
+
 export default App;
